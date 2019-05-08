f68217dbab5b9c7b5944f17c0969f4452a59adc1
you@example.com <nizamiislamovs@gmail.com>
2019-05-08 06:47:41 +0300

    Updated lib

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,6 +3,7 @@
 LIB_DIR_PATH="./../"
 export PROTEUS_LIB_PATH=~/".wine/drive_c/Program Files/Labcenter Electronics/Proteus 7 Professional/LIBRARY"
 
+#nautilus "$PROTEUS_LIB_PATH"
 /bin/cp -rf "$PROTEUS_LIB_PATH"/CUSTOM_CONNECTORS.* "$LIB_DIR_PATH"
 
 ls -la "$LIB_DIR_PATH" | grep "CUSTOM_CONNECTORS"
